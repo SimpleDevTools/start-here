f4562881fa6b8616be4d68879e8d567638beab88
Abdel Elrafa <pro@boca.pro>
2023-03-18 18:49:28 -0400

    Install and Configure Filament PHP Admin Panel with the admin_users Auth Guard.

--- app/Models/AdminUser.php
+++ app/Models/AdminUser.php
@@ -2,14 +2,20 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Notifications\Notifiable;
 
-class AdminUser extends User
+class AdminUser extends User implements FilamentUser
 {
     use Notifiable;
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
     protected $hidden = ['password', 'remember_token'];
+
+    public function canAccessFilament(): bool
+    {
+        return true;
+    }
 }
