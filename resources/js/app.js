9b942ddfa0b879019e586167b9ab0e0825b92d4e
Abdel Elrafa <pro@boca.pro>
2023-04-05 11:06:11 -0400

    Add livewire hot reload.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import "./bootstrap";
 
+import { livewire_hot_reload } from "virtual:livewire-hot-reload";
+livewire_hot_reload();
+
 import Alpine from "alpinejs";
 import FormsAlpinePlugin from "../../vendor/filament/forms/dist/module.esm";
 import NotificationsAlpinePlugin from "../../vendor/filament/notifications/dist/module.esm";
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import laravel, { refreshPaths } from "laravel-vite-plugin";
+import livewire, { defaultWatches } from "@defstudio/vite-livewire-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: [
+            refresh: false,
+        }),
+
+        livewire({
+            refresh: ["resources/css/app.css"],
+            watch: [
+                ...defaultWatches,
                 ...refreshPaths,
                 "app/Http/Livewire/**",
                 "app/Forms/Components/**",
