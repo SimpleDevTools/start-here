55ee922b0d657c98961341e7561a3658eeb59280
Abdel Elrafa <AbdelElrafa@users.noreply.github.com>
2023-11-28 20:36:47 -0500

    Install Vite Livewire Plugin.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,12 @@
 import "./bootstrap";
 
+import.meta.glob(["../images/**"]);
+
 import {
     Livewire,
     Alpine,
 } from "../../vendor/livewire/livewire/dist/livewire.esm";
-
 Livewire.start();
+
+import { livewire_hot_reload } from "virtual:livewire-hot-reload";
+livewire_hot_reload();
