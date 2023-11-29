b3474c5e824f15053be073eb0f0cf06b0606002f
Abdel Elrafa <AbdelElrafa@users.noreply.github.com>
2023-11-28 20:26:23 -0500

    Add typehint.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        /** @phpstan-ignore-next-line */
+        /** @var \Illuminate\Foundation\Vite $this */
         Vite::macro('image', fn (string $asset) => $this->asset("resources/images/{$asset}"));
     }
 }
