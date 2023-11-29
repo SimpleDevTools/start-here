3417a4d1671cb14ab915346ea4a5c5946ac507ee
Abdel Elrafa <AbdelElrafa@users.noreply.github.com>
2023-11-28 20:31:19 -0500

    Update user helper to report the exception so you know update the code.

--- app/helpers.php
+++ app/helpers.php
@@ -1,15 +1,23 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\SessionGuard;
+use Illuminate\Support\Facades\Auth;
+use Smpita\TypeAs\TypeAs;
 
 if (! function_exists('user')) {
     function user(string $guard = 'web'): User
     {
-        $user = auth($guard)->user();
-        if (! $user instanceof User) {
-            throw new RuntimeException('No user authenticated.');
-        }
+        try {
+            return TypeAs::class(
+                User::class,
+                TypeAs::class(SessionGuard::class, Auth::guard($guard))->authenticate()
+            );
+        } catch (AuthenticationException $e) {
+            report(new RuntimeException('User not authenticated.', previous: $e));
 
-        return $user;
+            throw $e;
+        }
     }
 }
