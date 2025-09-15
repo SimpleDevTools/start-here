d69440cdeb50835a64398a3289d9117307adaf8e
Ahmed Hazem <hazem@boca.pro>
2025-09-15 11:38:20 +0300

    Update project configuration and dependencies
    - Changed project name from "include-cloud-laravel" to "start-here" in package-lock.json.
    - Added `@defstudio/vite-livewire-plugin` as a dependency in both package.json and package-lock.json.
    - Updated vite.config.js to include the livewire plugin with specific refresh settings.
    - Enhanced app.js to enable livewire hot reloading functionality.

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,18 @@ import {
 } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
+import livewire from '@defstudio/vite-livewire-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: false,
         }),
         tailwindcss(),
+        livewire({
+            refresh: ['resources/css/app.css'],
+        }),
     ],
     server: {
         cors: true,
