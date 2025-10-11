ba193dde2d78626ea71d6da79c940e712e0a73a3
Abdel Elrafa <AbdelElrafa@users.noreply.github.com>
2025-10-11 10:06:40 -0400

    Update dependencies and configuration for Vite Livewire integration
    - Added @defstudio/vite-livewire-plugin version 3.0.2 to package.json and package-lock.json.
    - Updated vite.config.js to include the Livewire plugin with a refresh configuration.
    - Enhanced app.js to support livewire hot reloading functionality.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import.meta.glob(["../images/**"]);
 
+import { livewire_hot_reload } from "virtual:livewire-hot-reload";
+livewire_hot_reload();
+
 import "../../vendor/wire-elements/pro/resources/js/overlay-component.js";
 
 Alpine.directive("open-modal", (el, { expression }, { evaluate }) => {
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import livewire from "@defstudio/vite-livewire-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: false,
         }),
         tailwindcss(),
+        livewire({
+            refresh: ["resources/css/app.css"],
+        }),
     ],
     server: {
         cors: true,
