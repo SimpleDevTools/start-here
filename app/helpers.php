e2f42ef5da2a44c6086b6f4ea80ccfd3f02a7621
Abdel Elrafa <AbdelElrafa@users.noreply.github.com>
2025-10-24 07:23:02 -0400

    Update user helper to be more generic.

--- app/helpers.php
+++ app/helpers.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 use App\Models\User;
 use Illuminate\Auth\AuthenticationException;
-use Illuminate\Auth\SessionGuard;
+use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Support\Facades\Auth;
 use Smpita\TypeAs\TypeAs;
 
 if (! function_exists('user')) {
-    function user(string $guard = 'web'): User
+    function user(?string $guard = null): User
     {
         try {
             return TypeAs::class(
                 User::class,
-                TypeAs::class(SessionGuard::class, Auth::guard($guard))->authenticate()
+                TypeAs::class(Guard::class, Auth::guard($guard))->authenticate()
             );
         } catch (AuthenticationException $e) {
             report(new RuntimeException('User not authenticated.', previous: $e));
